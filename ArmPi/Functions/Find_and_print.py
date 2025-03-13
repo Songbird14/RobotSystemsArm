91b8e4bc80129fb557359214e8e8fb1e8a5e7f0a
Songbird14 <leah.hanen.5571@gmail.com>
2025-03-13 15:38:11 -0700

    Updating arm code

--- ArmPi/Functions/Find_and_print.py
+++ ArmPi/Functions/Find_and_print.py
@@ -4,16 +4,16 @@
 import ColorTrackingClass 
 import Perception_Class
 
-#ctc = ColorTrackingClass.ColorTracking()
-ctc =Perception_Class.ColorTracking()
+ctc = ColorTrackingClass.ColorTracking()
+#ctc =Perception_Class.ColorTracking()
 imag = ctc.find_color_block()
 
 ctc.display_info(imag)
     
 
-# ### movement 
+### movement 
 
-# ctc.pick_up_block()
+ctc.pick_up_block()
     
 
 
