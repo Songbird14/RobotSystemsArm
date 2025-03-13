525975630b4ac81981e3cfea51a05afca6efcf24
Songbird14 <leah.hanen.5571@gmail.com>
2025-03-12 18:50:51 -0700

    update arm code

--- ArmPi/Functions/Find_and_print.py
+++ ArmPi/Functions/Find_and_print.py
@@ -9,10 +9,9 @@ imag = ctc.find_color_block()
 
 processedImg = ctc.process_image(imag)
 
-world_x,world_y = ctc.photo_2_realWorld(processedImg,)
+world_x,world_y = ctc.photo_2_realWorld(processedImg)
 
 ctc.display_info(processedImg)
-
 
 
 
