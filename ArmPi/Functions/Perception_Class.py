f8d77cd9be5834289cb26c6353ef0f2b4b98b9fc
Songbird14 <leah.hanen.5571@gmail.com>
2025-03-13 15:33:04 -0700

    Updating arm code

--- ArmPi/Functions/Perception_Class.py
+++ ArmPi/Functions/Perception_Class.py
@@ -190,24 +190,24 @@ class ColorTracking():
                     continue
 
 
-    def judgement(self,world_x,world_y,distance):
-        if distance < 0.5:
-                    count += 1
-                    center_list.extend((world_x, world_y))
-                    if start_count_t1:
-                        start_count_t1 = False
-                        t1 = time.time()
-                    if time.time() - t1 > 1:
-                        self.rotation_angle = self.rect[2] 
-                        start_count_t1 = True
-                        self.world_X, self.world_Y = np.mean(np.array(center_list).reshape(count, 2), axis=0)
-                        center_list = []
-                        count = 0
-                        self.start_pick_up = True
-        else:
-            t1 = time.time()
-            start_count_t1 = True
-            center_list = []
-            count = 0 
+    # def judgement(self,world_x,world_y,distance):
+    #     if distance < 0.5:
+    #                 count += 1
+    #                 center_list.extend((world_x, world_y))
+    #                 if start_count_t1:
+    #                     start_count_t1 = False
+    #                     t1 = time.time()
+    #                 if time.time() - t1 > 1:
+    #                     self.rotation_angle = self.rect[2] 
+    #                     start_count_t1 = True
+    #                     self.world_X, self.world_Y = np.mean(np.array(center_list).reshape(count, 2), axis=0)
+    #                     center_list = []
+    #                     count = 0
+    #                     self.start_pick_up = True
+    #     else:
+    #         t1 = time.time()
+    #         start_count_t1 = True
+    #         center_list = []
+    #         count = 0 
    
 
