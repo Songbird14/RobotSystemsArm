071871c34cf067896051ece5cc609ec08dcad02f
Songbird14 <leah.hanen.5571@gmail.com>
2025-03-12 19:03:37 -0700

    update arm code

--- ArmPi/Functions/Find_and_print.py
+++ ArmPi/Functions/Find_and_print.py
@@ -5,19 +5,15 @@ import ColorTrackingClass
 
 ctc = ColorTrackingClass.ColorTracking()
 
-def percetion ():
-    imag = ctc.find_color_block()
+imag = ctc.find_color_block()
 
-    processedImg = ctc.process_image(imag)
+processedImg = ctc.process_image(imag)
 
-    world_x,world_y = ctc.photo_2_realWorld(processedImg)
+world_x,world_y = ctc.photo_2_realWorld(processedImg)
 
-    ctc.display_info(processedImg)
-
-    return processedImg
+ctc.display_info(processedImg)
     
-
-Frame = percetion()
+    
 
 
 
